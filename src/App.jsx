335eeefc3957b8c5c04807d097722b08caf6a33a
macnemoto <marcosj465@gmail.com>
2024-06-01 22:58:27 -0400

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import { Card, CardBody } from "@nextui-org/react";
 
 
 function App() {
+  const [message, setMessage] = useState([])
 const [data, setData] = useState([])
 
   useEffect(() => {
     const Api = async () => {
       try {
-        const res = await fetch("https://8chan.moe/arepa/res/83464.json");
+        const res = await fetch("https://8chan.moe/arepa/res/83493.json");
         const data = await res.json();
+        setMessage(data.message)
         setData(data.posts)
-        // console.log(data.posts);
+        console.log(data);
       } catch (error) {
         console.error("Error", error);
       }
@@ -34,12 +36,22 @@ const textH = (item) => {
     <>
       <div className="flex flex-col justify-center items-center pt-10">
         <h1 className="text-white text-6xl mb-5">Post de los Negritos</h1>
+        <Card className="w-1/2 bg-[#18181B] my-4">
+            <CardBody className="text-white bg-[#a83030]">
+            <div className="flex justify-between">
+            <p>{message}</p>
+              <div onClick={()=> (textH(message))} className="flex justify-center items-center self-start bg-white text-black p-4  rounded-lg mx-5">
+                <FaCaretRight/>
+              </div>
+            </div>
+            </CardBody>
+          </Card>
         {data.map((item, index) => (
           <Card key={index} className="w-1/2 bg-[#18181B] my-4">
             <CardBody className="text-white bg-[#18181B]">
             <div className="flex justify-between">
             <p>{item.message}</p>
-              <div onClick={()=> (textH(item.message))} className="flex justify-center items-center self-end bg-white text-black p-4  rounded-lg mx-5">
+              <div onClick={()=> (textH(item.message))} className="flex justify-center items-center self-start bg-white text-black p-4  rounded-lg mx-5">
                 <FaCaretRight/>
               </div>
             </div>
